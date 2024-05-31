52d56bc5bb253d03827017e5e1549f70c449e91e
cubejun <133946040+cubejun@users.noreply.github.com>
2024-05-31 07:09:59 -0700

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "avoid_vm/vm.hpp"
+int main(int argc, char **argv) {
+  rclcpp::init(argc, argv);
+
+  auto node = std::make_shared<VM>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+
+
+  return 0;
+}
